0502764cf271c1b1e62edab5409ad88cfd858a39
Riean <rieder0242@protonmail.com>
2024-07-10 17:03:38 +0200

    db config spring contxt test

--- src/main/java/hu/riean/personalregistry/Personalregistry.java
+++ src/main/java/hu/riean/personalregistry/Personalregistry.java
@@ -1,16 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package hu.riean.personalregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author riean
  */
+@SpringBootApplication
 public class Personalregistry {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Personalregistry.class, args);
     }
 }
---
+++ src/test/java/hu/riean/personalregistry/PersonalregistryTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package hu.riean.personalregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author riean
+ */
+@SpringBootTest
+public class PersonalregistryTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
