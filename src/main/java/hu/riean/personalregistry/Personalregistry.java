0eb1cc51616a9f41826910736ae65eb6850bc22a
Riean <rieder0242@protonmail.com>
2024-07-10 16:36:08 +0200

    init

---
+++ src/main/java/hu/riean/personalregistry/Personalregistry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hu.riean.personalregistry;
+
+/**
+ *
+ * @author riean
+ */
+public class Personalregistry {
+
+    public static void main(String[] args) {
+
+    }
+}
