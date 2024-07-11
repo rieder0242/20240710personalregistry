181af02255e77dd685a4564ec7880d6152fbc228
Riean <rieder0242@protonmail.com>
2024-07-12 00:04:30 +0200

    person view 3

--- src/main/resources/static/js/personOne.js
+++ src/main/resources/static/js/personOne.js
@@ -1,5 +1,65 @@
 /* global doT */
 
+class Contact {
+
+    constructor(value, element, parent) {
+        this.parent = parent;
+        let div = document.createElement('div');
+        div.innerHTML = templates.contact({data: value});
+        this.element = div.firstElementChild;
+        element.appendChild(this.element);
+        this.element.querySelector("button").addEventListener("click", (e) =>this.delete());
+        this.value = this.element.querySelector('[type="text"]');
+        this.kind = this.element.querySelector('select');
+
+        this.setValue(value);
+    }
+    setValue(value) {
+        this.value.value = value.value;
+        this.kind.value = value.kind;
+    }
+    getValue() {
+        return {
+            value: this.value.value,
+            kind: this.kind.value
+        };
+    }
+    delete(){
+        this.element.remove();
+        this.parent.removeChild(this);
+    }
+}
+class ContactList {
+
+    constructor(value, element) {
+        this.children = [];
+        this.element = element;
+        element.parentElement.querySelector("button").addEventListener("click", (e) => {
+            this.addChild({
+                kind: 'email',
+                value: ''
+            });
+        });
+        value.forEach((v) => {
+            this.addChild(v);
+        });
+    }
+    addChild(v) {
+        this.children.push(new Contact(v, this.element, this));
+    }
+    removeChild(child){
+        const index = this.children.indexOf(child);
+        this.children.splice(index, 1);
+        console.log(this.children);
+    }
+    getValue() {
+        let value = [];
+        this.children.forEach((e) => {
+            value.push(e.getValue());
+        });
+        return value;
+    }
+}
 
 class Address {
     constructor(value, element) {
@@ -17,12 +77,26 @@ class Address {
             this.updateExist();
         });
         this.updateExist();
+        let contactListE = element.querySelector('.contactList');
+        this.contacts = new ContactList(value.contacts, contactListE);
         this.setValue(value);
     }
     setValue(value) {
         this.zipCode.value = value.zipCode;
         this.settlement.value = value.settlement;
         this.address.value = value.address;
+    }
+    getValue() {
+        if (this.checkbox.checked) {
+            return {
+                zipCode: this.zipCode.value,
+                settlement: this.settlement.value,
+                address: this.address.value,
+                contacts: this.contacts.getValue()
+            };
+        } else {
+            return null;
+        }
     }
     getEmptyValue() {
         return  {
@@ -32,9 +106,9 @@ class Address {
             contacts: []
         };
     }
-    updateExist(){
-        if(this.checkbox.checked) {
-            this.hider.style.display = '';            
+    updateExist() {
+        if (this.checkbox.checked) {
+            this.hider.style.display = '';
         } else {
             this.hider.style.display = 'none';
         }
@@ -46,6 +120,7 @@ class PersonOne {
         this.id = document.getElementById('id').value;
         this.new = this.id == '';
         this.personE = document.getElementById("person");
+
         this.load();
     }
     async load() {
@@ -54,8 +129,7 @@ class PersonOne {
             if (this.new) {
                 json = {
                     firstName: '',
-                    lastName: '',
-                    addresses: []
+                    lastName: ''
                 };
             } else {
                 const url = "/api/person/" + this.id;
@@ -69,13 +143,40 @@ class PersonOne {
             console.log({data: json});
             this.personE.innerHTML = templates.person({data: json, new : this.new});
 
+            document.querySelector(".button.save").addEventListener("click", (e) => this.save());
+            document.querySelectorAll(".button.del").forEach((e) => e.addEventListener("click", (e) => this.delete()));
+
+
+            [this.lastName, this.firstName] = this.personE.querySelectorAll('[type="text"]');
+
             this.constant = new Address(json.constantAddress, document.getElementById('constant'));
-            this.constant = new Address(json.temporaryAddress, document.getElementById('temporary'));
+            this.temporary = new Address(json.temporaryAddress, document.getElementById('temporary'));
+
+            this.setValue(json);
 
         } catch (error) {
             console.log(error);
             document.getElementById("content").innerHTML = templates.error({message: "error"});
         }
+    }
+    setValue(value) {
+        this.lastName.value = value.lastName;
+        this.firstName.value = value.firstName;
+    }
+    getValue() {
+        return {
+            lastName: this.lastName.value,
+            firstName: this.firstName.value,
+            constant: this.constant.getValue(),
+            temporary: this.temporary.getValue()
+        }
+    }
+    async save() {
+        let value = this.getValue();
+        console.log(value);
+    }
+    async delete() {
+
     }
 
 }
